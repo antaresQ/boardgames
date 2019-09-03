10477e35f9eaa4750aa3e7e66fdb9f7cb22c438e
Harbinder <harbinder03@gmail.com>
2019-09-03 13:44:27 +0800

    Create DB connection

---
+++ backend/main.js
@@ -0,0 +1,32 @@
+// Load libraries
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
+// Configure PORT
+const PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000;
+
+// Load DB configuration 
+const config = require('./config.json');
+const URL = config.mongo || 'mongodb://localhost:27017'
+
+// Create an instance of MongoClient
+const client = new MongoClient(URL, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// Create an instance of the application
+const app = express();
+
+
+// connect to mongo/boardgames
+client.connect(
+    (err, client) => {
+        if (err) {
+            console.error('fail to restaurant:', err)
+            return;
+        }
+        console.info('connected to restaurant')
+        // Start the server
+        app.listen(PORT, () => {
+            console.info(`Application started on port ${PORT} at ${new Date()}`);
+        })
+    }
+)
