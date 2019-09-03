f9ab87102b764b8840f8e9d83a888e23c7009bb7
Harbinder <harbinder03@gmail.com>
2019-09-03 14:24:12 +0800

    Add components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchbynameComponent } from './components/searchbyname.component';
+import { SearchbycategoryComponent } from './components/searchbycategory.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchbynameComponent,
+    SearchbycategoryComponent
   ],
   imports: [
     BrowserModule
