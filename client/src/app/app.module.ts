6bc27a2d4f62d28b19734931be9bbb1f6a2632c2
Harbinder <harbinder03@gmail.com>
2019-09-05 14:07:23 +0800

    Add Gamelist Component and route

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,23 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { MaterialModule } from './material.module';
 
+import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
-import { SearchbynameComponent } from './components/searchbyname.component';
-import { SearchbycategoryComponent } from './components/searchbycategory.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GamelistComponent } from './components/gamelist.component';
+import { ApprouteModule } from './approute.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchbynameComponent,
-    SearchbycategoryComponent
+    GamelistComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule, ApprouteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- client/src/app/approute.module.ts
+++ client/src/app/approute.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from '@angular/router';
+import { GamelistComponent } from './components/gamelist.component';
 
 const ROUTES: Routes = [
-    
+    { path: 'games', component: GamelistComponent }
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(ROUTES)],
     exports: [RouterModule]
 })
+
 export class ApprouteModule {}
