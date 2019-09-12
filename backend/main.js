0378f22067c32773a5279342874a72ce39215c85
antaresQ <antares_elementium@hotmail.com>
2019-09-12 18:42:41 +0800

    all api calls for views on node done

--- backend/main.js
+++ backend/main.js
@@ -35,13 +35,107 @@ app.get('/api/games',
 
 )
 
+//GET /search games by name
+app.get('/api/gamesbyname/:name',
+    (req,resp) => {
+        client.db('boardgames')
+        .collection('gameslist')
+        .find({
+            Name: {$regex: req.params.name}
+        })
+        .toArray()
+        .then(result => {
+            resp.status(200)
+            resp.type('application/json')
+            resp.json(result)
+        })
+        .catch(error=> {
+            resp.status(400)
+            resp.end(error)
+        })
+    }
+)
+
+//GET /search games by single category
+//may need to paginate or slice
+app.get('/api/gamesbycategory/:category',
+    (req,resp) => {
+        client.db('boardgames')
+        .collection('gamesdetail')
+        .aggregate(
+            [
+                {  $lookup: {
+                    from: "gameslist",
+                    localField: "id",
+                    foreignField: "ID",
+                    as: "boardgamelistinfo"
+                    }
+                },
+                {
+                    $match: {
+                        boardgamecategory: { $regex: req.params.category}
+                    }
+                },
+                {
+                    $project: {
+                        id: 1,
+                        boardgamelistinfo: 1
+                    }
+                }
+            ]
+        )
+        .toArray()
+        .then(result => {
+            resp.status(200)
+            resp.type('application/json')
+            resp.json(result)
+        })
+        .catch(error=> {
+            resp.status(400)
+            resp.end(error)
+        })
+    }
+)
+
+//GET full game details by Id
+app.get('/api/game/:gameId',
+    (req,resp) => {
+        client.db('boardgames')
+        .collection('gameslist')
+        .aggregate(
+            [
+                {
+                    $match: {ID: Number(req.params.gameId, 10)}
+                },
+                {  $lookup: {
+                    from: "gamesdetail",
+                    localField: "ID",
+                    foreignField: "id",
+                    as: "boardgamedetails"
+                    }
+                }
+            ]
+        )
+        .toArray()
+        .then(result => {
+            resp.status(200)
+            resp.type('application/json')
+            resp.json(result)
+        })
+        .catch(error=> {
+            resp.status(400)
+            resp.end(error)
+        })
+    }
+)
+
 
 //GET /game comments
-app.get('/api/comments/:boardgameId',
+app.get('/api/comments/:gameId',
     (req,resp) => {
         client.db('boardgames')
         .collection('review')
-        .find({ID:Number(req.params.boardgameId)})
+        .find({ID:Number(req.params.gameId)})
         .toArray()
         .then(result => {
             resp.status(200)
